8ff728e02806644f9ab48379446876a5cfaf3690
preranakhare <prerana.khare@studytafensw.edu.au>
2025-07-10 13:34:50 +1000

    Update Jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,6 +4,7 @@ const config = {
   testMatch: ["**/tests/**/*.test.js"],
   testEnvironment: "node",
   transform: {}, // no Babel transform needed
-};
 
+  reporters: ["github-actions"], //  This line enables annotations
+};
 export default config;
