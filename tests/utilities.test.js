3b91162201e897887292d1d96a297e018a4a0b98
preranakhare <prerana.khare@studytafensw.edu.au>
2025-07-10 14:11:35 +1000

    Fix ESLint and test errors to pass pipeline

--- tests/utilities.test.js
+++ tests/utilities.test.js
@@ -1,9 +1,9 @@
 import { isValidDateString, wrapString } from "../src/utilities";
 import { myFunc } from "../src/utilities.js";
 
-test("failing test", () => {
-  expect(1).toBe(2); // To check Annotation is working
-});
+//test("failing test", () => {
+//expect(1).toBe(2); // To check Annotation is working
+//});
 
 test("wrapString: wrap single word over two lines", () => {
   expect(wrapString("Long", 3)).toBe("Lo-\nng");
